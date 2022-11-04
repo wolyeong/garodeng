8a18c45c154f29aca4954ea47c5afaafb02bbcc6
wolyeong <117245565+wolyeong@users.noreply.github.com>
2022-11-04 10:12:33 +0900

    main.blocks, main.ts 업데이트하기

--- main.ts
+++ main.ts
@@ -1,6 +1,6 @@
 let strip = neopixel.create(DigitalPin.P1, 1, NeoPixelMode.RGB)
 basic.forever(function () {
-    if (50 >= smarthome.ReadLightIntensity(AnalogPin.P2)) {
+    if (20 >= smarthome.ReadLightIntensity(AnalogPin.P2)) {
         strip.showColor(neopixel.colors(NeoPixelColors.White))
     } else {
         strip.showColor(neopixel.colors(NeoPixelColors.Black))
